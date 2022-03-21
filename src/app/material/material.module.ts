cdba015293cf6f480757151fd68a78032dde7177
rehmanmemon12 <rehmanmemon007@gmail.com>
2022-03-22 00:10:44 +0500

    Add Angular Material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import {MatButtonModule} from '@angular/material/button';
+
+const MaterialComponent = [
+  MatButtonModule
+];
+
+@NgModule({
+  imports: [MaterialComponent],
+  exports: [MaterialComponent]
+})
+export class MaterialModule { }
