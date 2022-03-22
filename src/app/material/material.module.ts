904e2c1766241b4c00b51bdd97881269a4d0e712
rehmanmemon12 <rehmanmemon007@gmail.com>
2022-03-23 04:14:23 +0500

    TypoGray Material Button

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
 
 const MaterialComponent = [
-  MatButtonModule
-];``
+  MatButtonModule,
+  MatButtonToggleModule
+];
 
 @NgModule({
   imports: [MaterialComponent],
